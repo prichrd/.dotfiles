9c72606d6df9362613b127a4cd4301384b1277c2
Philippe Richard <prichrd.dev@gmail.com>
2022-10-15 12:14:55 -0400

    updates

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,20 +3,5 @@ if require "first_load"() then
 end
 
 require('plugins')
+require('options')
 require('impatient')
-
-vim.g.mapleader = [[ ]]
-
-vim.opt.cursorline = true
-vim.opt.expandtab = true
-vim.opt.foldlevel = 20
-vim.opt.list = true
-vim.opt.mouse = 'a'
-vim.opt.number = true
-vim.opt.shell = 'zsh'
-vim.opt.shiftwidth = 2
-vim.opt.swapfile = false
-vim.opt.tabstop = 2
-vim.opt.termguicolors = true
-vim.opt.winbar = "%f"
-vim.opt.ch = 0
---
+++ nvim/.config/nvim/lua/options.lua
@@ -0,0 +1,15 @@
+vim.g.mapleader = [[ ]]
+
+vim.opt.cursorline = true
+vim.opt.expandtab = true
+vim.opt.foldlevel = 20
+vim.opt.list = true
+vim.opt.mouse = 'a'
+vim.opt.number = true
+vim.opt.shell = 'zsh'
+vim.opt.shiftwidth = 2
+vim.opt.swapfile = false
+vim.opt.tabstop = 2
+vim.opt.termguicolors = true
+vim.opt.winbar = "%f"
+vim.opt.ch = 0
