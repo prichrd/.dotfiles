9ec3a709f4fc867ffe65f3340f59a285761154f0
Philippe Richard <prichrd.dev@gmail.com>
2024-03-05 16:26:31 -0500

    updates

--- nvim/.config/nvim/lua/pr/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/pr/plugins/treesitter.lua
@@ -7,13 +7,16 @@ return {
   },
   {
     "nvim-treesitter/nvim-treesitter",
-    ft = { "go", "gomod", "rust", "lua", "javascript", "typescript", "http", "json", "r", "python" },
+    ft = { "go", "gomod", "lua" },
     config = function()
       require("nvim-treesitter.configs").setup({
-        ensure_installed = { "go", "gomod", "rust", "lua", "typescript", "http", "json", "r", "python" },
+        ensure_installed = { "go", "gomod", "lua" },
         highlight = {
           enable = true,
         },
+        sync_install = false,
+        auto_install = true,
+        ignore_install = {},
         disable = function(_, buf)
           local max_filesize = 100 * 1024 -- 100 KB
           local exists, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
@@ -21,6 +24,7 @@ return {
             return true
           end
         end,
+        additional_vim_regex_highlighting = false,
       })
       vim.opt.foldmethod = "expr"
       vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
