29bcd719fd427dd7156acabee03501a13ec9f57f
Philippe Richard <prichrd.dev@gmail.com>
2024-04-18 18:27:12 -0400

    updates

--- nvim/.config/nvim/lua/pr/keymaps.lua
+++ nvim/.config/nvim/lua/pr/keymaps.lua
@@ -3,3 +3,6 @@ vim.keymap.set("n", "[b", ":bp<cr>")
 vim.keymap.set("n", "]b", ":bn<cr>")
 vim.keymap.set("n", "[t", ":tabp<cr>")
 vim.keymap.set("n", "]t", ":tabn<cr>")
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+
--- nvim/.config/nvim/lua/pr/plugins.lua
+++ nvim/.config/nvim/lua/pr/plugins.lua
@@ -4,6 +4,46 @@ require("lazy").setup({
   "tpope/vim-vinegar",
   "tpope/vim-surround",
   "tpope/vim-commentary",
+  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      {
+        "<Leader>ha",
+        function()
+          local harpoon = require("harpoon")
+          harpoon:list():add()
+        end,
+        desc = "Add a harpoon",
+      },
+      {
+        "<Leader>hh",
+        function()
+          local harpoon = require("harpoon")
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = "Add a harpoon",
+      },
+      {
+        "]h",
+        function()
+          local harpoon = require("harpoon")
+          harpoon:list():next()
+        end,
+        desc = "Next harpoon",
+      },
+      {
+        "[h",
+        function()
+          local harpoon = require("harpoon")
+          harpoon:list():prev()
+        end,
+        desc = "Next harpoon",
+      },
+    },
+  },
 
   require("pr.plugins.colorscheme"),
   require("pr.plugins.git"),
