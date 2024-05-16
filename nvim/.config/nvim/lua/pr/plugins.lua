fc15e6c80e6ad89bddbfd84ab4691d168dc294a5
Philippe Richard <prichrd.dev@gmail.com>
2024-05-15 20:55:03 -0400

    updates

--- nvim/.config/nvim/lua/pr/plugins.lua
+++ nvim/.config/nvim/lua/pr/plugins.lua
@@ -3,76 +3,6 @@ require("lazy").setup({
   "nvim-tree/nvim-web-devicons",
   "tpope/vim-surround",
   "tpope/vim-commentary",
-  {
-    "stevearc/oil.nvim",
-    opts = {
-      view_options = {
-        show_hidden = false,
-        is_hidden_file = function(name, bufnr)
-          return name == ".."
-        end,
-      },
-      keymaps = {
-        ["<TAB>"] = function()
-          local oil = require('oil')
-          require('vwd').set_vwd(oil.get_current_dir(), true)
-          print("[oil] current vwd:", require('vwd').get_vwd())
-        end,
-      },
-    },
-    keys = {
-      {
-        "-",
-        function()
-          require("oil").open()
-        end,
-      }
-    },
-  },
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    opts = {},
-  },
-  {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    keys = {
-      {
-        "<Leader>ha",
-        function()
-          local harpoon = require("harpoon")
-          harpoon:list():add()
-        end,
-        desc = "Add a harpoon",
-      },
-      {
-        "<Leader>hh",
-        function()
-          local harpoon = require("harpoon")
-          harpoon.ui:toggle_quick_menu(harpoon:list())
-        end,
-        desc = "Add a harpoon",
-      },
-      {
-        "]h",
-        function()
-          local harpoon = require("harpoon")
-          harpoon:list():next()
-        end,
-        desc = "Next harpoon",
-      },
-      {
-        "[h",
-        function()
-          local harpoon = require("harpoon")
-          harpoon:list():prev()
-        end,
-        desc = "Next harpoon",
-      },
-    },
-  },
 
   require("pr.plugins.ai"),
   require("pr.plugins.colorscheme"),
@@ -81,9 +11,11 @@ require("lazy").setup({
   require("pr.plugins.lsp"),
   require("pr.plugins.luasnip"),
   require("pr.plugins.mini"),
+  require("pr.plugins.nav"),
   require("pr.plugins.prichrd"),
   require("pr.plugins.telescope"),
   require("pr.plugins.treesitter"),
+  require("pr.plugins.visual"),
 }, {
   dev = {
     path = "~/Workspace/github.com/prichrd/",
--- nvim/.config/nvim/lua/pr/plugins/prichrd.lua
+++ nvim/.config/nvim/lua/pr/plugins/prichrd.lua
@@ -1,19 +1,5 @@
 return {
   {
-    "prichrd/netrw.nvim",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    opts = {
-      mappings = {
-        ["p"] = function(payload)
-          require("vwd").set_vwd(payload.dir, true)
-          print("current vwd:", require("vwd").get_vwd())
-        end,
-      },
-    },
-  },
-  {
     "prichrd/vwd.nvim",
     opts={},
   },
