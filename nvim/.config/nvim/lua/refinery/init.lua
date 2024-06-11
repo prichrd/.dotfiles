5017b474b9d13efe2fd9a2c50e06da35272b4805
Philippe Richard <prichrd.dev@gmail.com>
2024-06-11 09:35:00 -0400

    ref

--- nvim/.config/nvim/lua/refinery/init.lua
+++ nvim/.config/nvim/lua/refinery/init.lua
@@ -1,29 +1,6 @@
---- @alias Location { path: string, line: number, column: number }
---- @alias StepKind "find_file" | "grep"
---- @alias Step { kind: StepKind, query: string, locations: Location[] }
+--- @alias File { path: string, locations: Location[] }
+--- @alias Location { line: number, column: number, text: string }
 
-local M = {
-  bufnr = -1,
-  locations = {},
-}
-
-
-M.run = function()
-  local job = vim.fn.jobstart(
-    'fd -c never --type file',
-    {
-      cwd = '.',
-      on_exit = function(jobid, data, event)
-      end,
-      on_stdout = function(jobid, data, event)
-        print("data", vim.inspect(data))
-      end,
-      on_stderr = function(jobid, data, event)
-      end
-    }
-  )
-end
-
-M.run()
+local M = {}
 
 return M
