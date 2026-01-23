41c54b9ef5d5eb6189f93e21628be9fee8a22297
Philippe Richard <prichrd.dev@gmail.com>
2026-01-23 13:40:57 -0500

    updates

--- nvim/.config/nvim/lua/core/plugins.lua
+++ nvim/.config/nvim/lua/core/plugins.lua
@@ -239,5 +239,9 @@ require("lazy").setup({
 	},
 	{
 		"github/copilot.vim",
+		config = function()
+			vim.g.copilot_no_tab_map = true
+			vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+		end,
 	},
 })
